4bed26deb7b1fd8ed8c3fe8c843119451707d8ee
Dave Yeagly <dave@yeags.com>
2020-09-07 15:51:54 -0700

    init

---
+++ pycalc.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+
+class CalcApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+    
+    def create_display(self, parent):
+        pass
+
+    def create_buttons(self):
+        pass
+
+if __name__ == '__main__':
+    app = CalcApp()
+    app.mainloop()
